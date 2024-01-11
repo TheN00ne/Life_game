2b53f7d0edf50f83c660c5f3af716f1b3230c670
unknown <Dima>
2024-01-11 23:35:01 +0200

    update 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,147 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { useState, useEffect } from 'react';
+
+import style from "./styles/style.module.css"
+
 const app = document.getElementById("app");
 
 function App(){
+
+  let [field, setField] = useState([]);
+  let [second, setSecond] = useState(field);
+  let [id, setId] = useState(0);
+  let [isStart, setStart] = useState(false);
+
+  function change(cell){
+    setField(field.map((row) => (
+      row.map((el) => {
+        if((el.id == cell.id) && (el.row == cell.row)){
+          return(
+            {id:el.id, isAlive:!el.isAlive, row:el.row}
+          )
+        }
+        else{
+          return el;
+        }
+      })
+    )))
+  }
+
+  useEffect(() =>
+   {
+    for(let i = 0; i < 25; i++){
+      let row = [];
+      for(let j = 0; j < 25; j++){
+        row.push({id:id, isAlive:false, row:i});
+        setId(id++);
+      }
+      field.push(row);
+    }
+   }, []);
+
+   function run(){
+      setSecond(field.map((row, id) => (
+        row.map((cell, inx) => {
+          let prevR;
+          let nextR;
+  
+          let prevC;
+          let nextC;
+  
+          if(id - 1 < 0){
+            prevR = 24;
+          }
+          else{
+            prevR = id - 1;
+          }
+  
+          if(id + 1 > 24){
+            nextR = 0;
+          }
+          else{
+            nextR = id + 1;
+          }
+          
+          if(inx - 1 < 0){
+            prevC = 24;
+          }
+          else{
+            prevC = inx - 1;
+          }
+  
+          if(inx + 1 > 24){
+            nextC = 0;
+          }
+          else{
+            nextC = inx + 1;
+          }
+  
+          let value = 0;
+  
+          if(field[prevR][prevC].isAlive == true){
+            value += 1;
+          }
+          if(field[prevR][inx].isAlive == true){
+            value += 1;
+          }
+          if(field[prevR][nextC].isAlive == true){
+            value += 1;
+          }
+  
+          if(field[id][prevC].isAlive == true){
+            value += 1;
+          }
+          if(field[id][nextC].isAlive == true){
+            value += 1;
+          }
+  
+          if(field[nextR][prevC].isAlive == true){
+            value += 1;
+          }
+          if(field[nextR][inx].isAlive == true){
+            value += 1;
+          }
+          if(field[nextR][nextC].isAlive == true){
+            value += 1;
+          }
+
+          if((value < 2) || (value > 3)){
+            return {id: inx, isAlive: false}
+          }
+          else{
+            return {id: inx, isAlive: true}
+          }
+        })
+      )))
+   }
+
+   useEffect(() => {
+    run();
+   }, [field])
+
+   useEffect(() => {
+      setField(second);
+   }, [isStart])
+
   return(
-    <div>
-      App
+    <div className={style.outside}>
+      <h1>Life game</h1>
+      <div className={style.field}>
+        {
+          field.map((row, inx) => (
+            <div className={style.row} key={inx}>
+              {
+                row.map((cell) => (
+                  <span className={`${cell.isAlive == true ? style.cellAlive : style.cellDead}`} tabIndex={1} key={cell.id} onMouseDown={() => {change(cell)}}   onKeyDown={(e) => {if(e.key == " "){change(cell)}}}></span>
+                ))
+              }
+            </div>
+          ))
+        }
+      </div>
+      <button onClick={() => {setStart(!isStart)}}>{isStart ? "Stop" : "Start"}</button>
     </div>
   )
 }
