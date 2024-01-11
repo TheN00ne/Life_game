93d9e6b40a8c658794958a695c998ce96ad73b04
unknown <Dima>
2024-01-11 19:06:10 +0200

    Begin

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById("app");
+
+function App(){
+  return(
+    <div>
+      App
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, app);
