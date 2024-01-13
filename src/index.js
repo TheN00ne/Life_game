f603e4e78ef913f1969839cb13aca0094ff7330b
unknown <Dima>
2024-01-13 13:29:27 +0200

    Update 3

--- src/index.js
+++ src/index.js
@@ -10,11 +10,10 @@ const app = document.getElementById("app");
 function App(){
 
   let [field, setField] = useState([]);
-  let [arr, setArr] = useState([]);
   let [id, setId] = useState(0);
-  let [start, setStart] = useState(false);
+  let [isStart, setIsStart] = useState(false);
   // let [speed, setSpeed] = useState(500);
-  let [inter, setInter] = useState(null);
+  let [timer, setTimer] = useState(null);
 
   function change(cell){
     setField(field.map((row) => (
@@ -44,108 +43,102 @@ function App(){
     }
    }, []);
 
-   function run(prev){
-      let newArr = prev.map((row, id) => {
-        let line = row.map((cell, inx) => {
-          let prevR;
-          let nextR;
-  
-          let prevC;
-          let nextC;
-  
-          if(id - 1 < 0){
-            prevR = row.length - 1;
-          }
-          else{
-            prevR = id - 1;
-          }
-  
-          if(id + 1 > row.length - 1){
-            nextR = 0;
-          }
-          else{
-            nextR = id + 1;
-          }
-          
-          if(inx - 1 < 0){
-            prevC = row.length - 1;
-          }
-          else{
-            prevC = inx - 1;
-          }
-  
-          if(inx + 1 > row.length - 1){
-            nextC = 0;
-          }
-          else{
-            nextC = inx + 1;
-          }
-  
-          let value = 0;
-  
-          if(field[prevR][prevC].isAlive == true){
-            value += 1;
-          }
-          if(field[prevR][inx].isAlive == true){
-            value += 1;
-          }
-          if(field[prevR][nextC].isAlive == true){
-            value += 1;
-          }
-  
-          if(field[id][prevC].isAlive == true){
-            value += 1;
-          }
-          if(field[id][nextC].isAlive == true){
-            value += 1;
-          }
-  
-          if(field[nextR][prevC].isAlive == true){
-            value += 1;
-          }
-          if(field[nextR][inx].isAlive == true){
-            value += 1;
-          }
-          if(field[nextR][nextC].isAlive == true){
-            value += 1;
-          }
-
-          if((cell.isAlive == false) && (value == 3)){
-            return {id: cell.id, isAlive: true, row:cell.row}
-          }
-
-          if((cell.isAlive == true) && ((value == 2) || (value == 3))){
-            return {id: cell.id, isAlive: true, row:cell.row}
-          }
-          else{
-            return {id: cell.id, isAlive: false, row:cell.row}
-          }
-        })
-        return line;
-   })
-   setField(newArr)
-   return(newArr);
-  }
-  
-  useEffect(() => {
-    setArr(run(field));
-  }, [field])
-
    useEffect(() => {
-    let id;
-    if(start){
-      id = setInterval(() => {
-        setField(run(arr))
-        // run(field);
-        console.log("+");
-      },200)
-      setInter(id);
+    console.log("aaa");
+    if (isStart) {
+      function run(prev) {
+        let newArr = prev.map((row, id) => {
+          let line = row.map((cell, inx) => {
+            let prevR;
+            let nextR;
+    
+            let prevC;
+            let nextC;
+    
+            if(id - 1 < 0){
+              prevR = row.length - 1;
+            }
+            else{
+              prevR = id - 1;
+            }
+    
+            if(id + 1 > row.length - 1){
+              nextR = 0;
+            }
+            else{
+              nextR = id + 1;
+            }
+            
+            if(inx - 1 < 0){
+              prevC = row.length - 1;
+            }
+            else{
+              prevC = inx - 1;
+            }
+    
+            if(inx + 1 > row.length - 1){
+              nextC = 0;
+            }
+            else{
+              nextC = inx + 1;
+            }
+    
+            let value = 0;
+    
+            if(field[prevR][prevC].isAlive == true){
+              value += 1;
+            }
+            if(field[prevR][inx].isAlive == true){
+              value += 1;
+            }
+            if(field[prevR][nextC].isAlive == true){
+              value += 1;
+            }
+    
+            if(field[id][prevC].isAlive == true){
+              value += 1;
+            }
+            if(field[id][nextC].isAlive == true){
+              value += 1;
+            }
+    
+            if(field[nextR][prevC].isAlive == true){
+              value += 1;
+            }
+            if(field[nextR][inx].isAlive == true){
+              value += 1;
+            }
+            if(field[nextR][nextC].isAlive == true){
+              value += 1;
+            }
+  
+            if((cell.isAlive == false) && (value == 3)){
+              return {id: cell.id, isAlive: true, row:cell.row}
+            }
+  
+            if((cell.isAlive == true) && ((value == 2) || (value == 3))){
+              return {id: cell.id, isAlive: true, row:cell.row}
+            }
+            else{
+              return {id: cell.id, isAlive: false, row:cell.row}
+            }
+          });
+          return line;
+        });
+        setField(newArr);
+        setTimer(setTimeout(() => run(newArr), 200));
+      }
+
+      run(field);
+
+      return () => {
+        clearTimeout(timer);
+      };
     }
-    else{
-      clearInterval(inter);
-      console.log("-");
+    if(!isStart){
+      clearTimeout(timer);
     }
-   }, [start])
+  }, [isStart, timer]);
 
   return(
     <div className={style.outside}>
@@ -163,8 +156,8 @@ function App(){
           ))
         }
       </div>
-      <button onClick={() => {run(field)}}>Next</button>
-      <button onClick={() => {setStart(!start)}}>Start</button>
+      {/* <button onClick={() => {run()}}>Next</button> */}
+      <button onClick={() => {setIsStart(!isStart)}}>Start</button>
       {/* <input type="range" min={100} max={5000} value={speed} onInput={(e) => {setSpeed(e.currentTarget.value)}}></input> */}
     </div>
   )
