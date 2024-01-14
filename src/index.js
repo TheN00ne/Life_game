9b053f53a9b60296fdfb7d8c7e2324f7158877f3
unknown <Dima>
2024-01-14 21:59:46 +0200

    The final update

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ function App(){
   let [field, setField] = useState([]);
   let [id, setId] = useState(0);
   let [isStart, setIsStart] = useState(false);
-  // let [speed, setSpeed] = useState(500);
   let [timer, setTimer] = useState(null);
+  let [steps, setSteps] = useState(0);
+  let [click, setClick] = useState(0);
 
   function change(cell){
     setField(field.map((row) => (
@@ -30,135 +31,160 @@ function App(){
     )))
   }
 
-  useEffect(() =>
-   {
-    for(let i = 0; i < 50; i++){
+  function random(){
+    setId(0);
+    setSteps(0);
+    let newField = [];
+    for(let i = 0; i < 60; i++){
       let row = [];
-      for(let j = 0; j < 50; j++){
+      for(let j = 0; j < 70; j++){
+        let is = Math.round(Math.random());
+        row.push({id:id, isAlive:is, row:i});
+        setId(id++);
+      }
+      newField.push(row);
+    }
+    setField(newField);
+  }
+
+  function clear(){
+    setSteps(0);
+    let newField = [];
+    for(let i = 0; i < 60; i++){
+      let row = [];
+      for(let j = 0; j < 70; j++){
         row.push({id:id, isAlive:false, row:i});
         setId(id++);
-        console.log("yey");
       }
-      field.push(row);
+      newField.push(row);
     }
-   }, []);
+    setField(newField);
+  }
+
+  useEffect(clear, []);
+
+   function run(prev) {
+    let newArr = prev.map((row, id) => {
+      let line = row.map((cell, inx) => {
+        let prevR;
+        let nextR;
+
+        let prevC;
+        let nextC;
+
+        if(id - 1 < 0){
+          prevR = field.length - 1;
+        }
+        else{
+          prevR = id - 1;
+        }
+
+        if(id + 1 > field.length - 1){
+          nextR = 0;
+        }
+        else{
+          nextR = id + 1;
+        }
+        
+        if(inx - 1 < 0){
+          prevC = row.length - 1;
+        }
+        else{
+          prevC = inx - 1;
+        }
+
+        if(inx + 1 > row.length - 1){
+          nextC = 0;
+        }
+        else{
+          nextC = inx + 1;
+        }
+
+        let value = 0;
+
+        if(field[prevR][prevC].isAlive == true){
+          value += 1;
+        }
+        if(field[prevR][inx].isAlive == true){
+          value += 1;
+        }
+        if(field[prevR][nextC].isAlive == true){
+          value += 1;
+        }
+
+        if(field[id][prevC].isAlive == true){
+          value += 1;
+        }
+        if(field[id][nextC].isAlive == true){
+          value += 1;
+        }
+
+        if(field[nextR][prevC].isAlive == true){
+          value += 1;
+        }
+        if(field[nextR][inx].isAlive == true){
+          value += 1;
+        }
+        if(field[nextR][nextC].isAlive == true){
+          value += 1;
+        }
+
+        if((cell.isAlive == false) && (value == 3)){
+          return {id: cell.id, isAlive: true, row:cell.row}
+        }
+
+        if((cell.isAlive == true) && ((value == 2) || (value == 3))){
+          return {id: cell.id, isAlive: true, row:cell.row}
+        }
+        else{
+          return {id: cell.id, isAlive: false, row:cell.row}
+        }
+      });
+      return line;
+    });
+    setField(newArr);
+    setSteps(steps + 1);
+
+    if(isStart){
+      setTimer(setTimeout(() => run(newArr), 200));
+    }
+  }
 
    useEffect(() => {
-    console.log("aaa");
     if (isStart) {
-      function run(prev) {
-        let newArr = prev.map((row, id) => {
-          let line = row.map((cell, inx) => {
-            let prevR;
-            let nextR;
-    
-            let prevC;
-            let nextC;
-    
-            if(id - 1 < 0){
-              prevR = row.length - 1;
-            }
-            else{
-              prevR = id - 1;
-            }
-    
-            if(id + 1 > row.length - 1){
-              nextR = 0;
-            }
-            else{
-              nextR = id + 1;
-            }
-            
-            if(inx - 1 < 0){
-              prevC = row.length - 1;
-            }
-            else{
-              prevC = inx - 1;
-            }
-    
-            if(inx + 1 > row.length - 1){
-              nextC = 0;
-            }
-            else{
-              nextC = inx + 1;
-            }
-    
-            let value = 0;
-    
-            if(field[prevR][prevC].isAlive == true){
-              value += 1;
-            }
-            if(field[prevR][inx].isAlive == true){
-              value += 1;
-            }
-            if(field[prevR][nextC].isAlive == true){
-              value += 1;
-            }
-    
-            if(field[id][prevC].isAlive == true){
-              value += 1;
-            }
-            if(field[id][nextC].isAlive == true){
-              value += 1;
-            }
-    
-            if(field[nextR][prevC].isAlive == true){
-              value += 1;
-            }
-            if(field[nextR][inx].isAlive == true){
-              value += 1;
-            }
-            if(field[nextR][nextC].isAlive == true){
-              value += 1;
-            }
-  
-            if((cell.isAlive == false) && (value == 3)){
-              return {id: cell.id, isAlive: true, row:cell.row}
-            }
-  
-            if((cell.isAlive == true) && ((value == 2) || (value == 3))){
-              return {id: cell.id, isAlive: true, row:cell.row}
-            }
-            else{
-              return {id: cell.id, isAlive: false, row:cell.row}
-            }
-          });
-          return line;
-        });
-        setField(newArr);
-        setTimer(setTimeout(() => run(newArr), 200));
-      }
-
       run(field);
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-    if(!isStart){
       clearTimeout(timer);
     }
-  }, [isStart, timer]);
+    else{
+      clearTimeout(timer);
+    }
+    }, [isStart, timer]);
 
   return(
-    <div className={style.outside}>
-      <h1>Life game</h1>
-      <div className={style.field}>
-        {
-          field.map((row, inx) => (
-            <div className={style.row} key={inx}>
-              {
-                row.map((cell) => (
-                  <span className={`${cell.isAlive == true ? style.cellAlive : style.cellDead}`} tabIndex={1} key={cell.id} onMouseDown={() => {change(cell)}}   onKeyDown={(e) => {if(e.code == "Enter"){change(cell)}}}></span>
-                ))
-              }
-            </div>
-          ))
-        }
+    <div>
+      <b className={style.steps}>Steps: {steps}</b>
+      <div className={style.outside}>
+        <h1>Life game</h1>
+        <div className={style.field}>
+          {
+            field.map((row, inx) => (
+              <div className={style.row} key={inx}>
+                {
+                  row.map((cell) => (
+                    <span className={`${cell.isAlive == true ? style.cellAlive : style.cellDead}`} onMouseEnter={(e) => {e.currentTarget.style.border = "1px dashed white"}} onMouseLeave={(e) => {e.currentTarget.style.border = ""}} tabIndex={1} key={cell.id} onMouseMove={() => {if(click){change(cell)}}} onMouseDown={() => {setClick(true)}} onMouseUp={() => {setClick(false)}} onClick={() => {change(cell)}} onKeyDown={(e) => {if(e.code == "Enter"){change(cell)}}}></span>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
       </div>
-      {/* <button onClick={() => {run()}}>Next</button> */}
-      <button onClick={() => {setIsStart(!isStart)}}>Start</button>
-      {/* <input type="range" min={100} max={5000} value={speed} onInput={(e) => {setSpeed(e.currentTarget.value)}}></input> */}
+      <div className={style.buttons}>
+        <button className={isStart ? `${style.disBut}` : `${style.but}`} disabled={isStart ? true : false} onClick={() => {run(field)}}>Next</button>
+        <button className={style.but} onClick={() => {setIsStart(!isStart)}}>{isStart ? "Stop" : "Start"}</button>
+        <button className={isStart ? `${style.disBut}` : `${style.but}`} disabled={isStart ? true : false} onClick={() => {random()}}>Random</button>
+        <button className={isStart ? `${style.disBut}` : `${style.but}`} disabled={isStart ? true : false} onClick={() => {clear()}}>Clear</button>
+      </div>
     </div>
   )
 }
